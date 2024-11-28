33fa792e264b5d6982b44f71b83bb94cd5c39ac3
Sambit Sargam Ekalabya <sambitsargam2003@gmail.com>
2024-11-28 14:11:18 +0530

    fixed error

--- backend/server.js
+++ backend/server.js
@@ -125,6 +125,13 @@ app.use((req, res, next) => {
     res.status(404).send({ error: 'Route not found' });
 });
 
+app.use(cors({
+    origin: 'https://aebot-web.vercel.app', // Allow only this domain
+    methods: ['GET', 'POST'], // Define allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Define allowed headers
+}));
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
